48c816ecbdd06ce730df3135e710dd73c2b51693
pacman2000 <60517952+pacman2000@users.noreply.github.com>
2020-03-04 13:11:11 +0100

    Add files via upload

---
+++ letters_phase03/scripts/scripts.js
@@ -0,0 +1,40 @@
+function mostrar(){
+  var myName = Array.from("AMPARO");
+  var newName = new Map();      
+  var letter;
+
+    //recorrem l'array
+  for (i = 0; i < myName.length; i++){
+    //guarda a "letter" el valor  del array indicat per la posició i
+    letter = myName[i];    
+    //si el valor de letter el troba dintre del map...   
+    if(newName.has(letter) == true){
+      //... llavors quantity recupera el valor (amb get) de letter i li suma 1
+      var quantity = newName.get(letter) +1;
+      //...i guarda (set) a newName els valors de letter i quantity
+      newName.set(letter, quantity);
+    }else{
+      //...en cas contrari guarda a newName el valor de letter i 1
+      newName.set(letter, 1);
+    }   
+  }
+
+  console.log("Partim del nom: " + myName.join(""));
+  console.log(newName);
+}
+
+
+
+//es pot fer tambè amb una map function
+/* 
+function mostrar(){
+  var myName = Array.from("AMPARO");
+  var newName = new Map();   
+  
+  myName.map(function(letter) {
+    newName[letter] = (newName[letter] || 0) + 1;
+  });
+
+  console.log("Partim del nom: " + myName.join(""));
+  console.log(newName);
+} */
